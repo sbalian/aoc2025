400f0d097bfb7ba9f30876be2b3952c0497d6ca0
Seto Balian <seto.balian@gmail.com>
2025-12-08 23:10:28 +0000

    Faster day 3 part 1

--- solutions/day03/solution.py
+++ solutions/day03/solution.py
@@ -1,22 +1,33 @@
 from pathlib import Path
 
 
-def batteries_for_max_joltage(bank: str) -> int:
-    max_ = 1
-    for i in range(len(bank)):
-        j = 0
-        while j < i:
-            max_ = current if (current := int(f"{bank[j]}{bank[i]}")) > max_ else max_
-            j += 1
-    return max_
+def max_from_left(bank: str) -> int:
+    max_pos, max_ = 0, int(bank[0])
+    for i in range(len(bank) - 1):
+        if (current := int(bank[i])) > max_:
+            max_pos, max_ = i, current
+    return max_pos
+
+
+def max_from_right(bank: str, end: int) -> int:
+    n = len(bank)
+    max_pos, max_ = n - 1, int(bank[n - 1])
+    for i in range(n - 1, end, -1):
+        if (current := int(bank[i])) > max_:
+            max_pos, max_ = i, current
+    return max_pos
+
+
+def max_joltage(bank: str) -> int:
+    left = max_from_left(bank)
+    right = max_from_right(bank, left)
+    return int(f"{bank[left]}{bank[right]}")
 
 
 def part1(path: str) -> int:
-    j = 0
-    for bank in Path(path).read_text().strip().splitlines():
-        m = batteries_for_max_joltage(bank)
-        j += m
-    return j
+    return sum(
+        max_joltage(bank) for bank in Path(path).read_text().strip().splitlines()
+    )
 
 
 def main() -> None:
