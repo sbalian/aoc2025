e6c53b4d5a75be6fd616702617507810af040a74
Seto Balian <seto.balian@gmail.com>
2025-12-13 19:05:30 +0000

    Alternative day 7 part 1

--- solutions/day07/solution.py
+++ solutions/day07/solution.py
@@ -1,118 +1,94 @@
 from collections import defaultdict
+from functools import cache
 from pathlib import Path
 
 Point = tuple[int, int]
 
 
-def read_grid(path: str) -> list[list[str]]:
-    return [list(line) for line in Path(path).read_text().strip().splitlines()]
+class Grid:
+    def __init__(self, path: str):
+        self.values = [
+            list(line) for line in Path(path).read_text().strip().splitlines()
+        ]
+        self.height = len(self.values)
+        self.width = len(self.values[0])
+        self.start = self.find_start()
+        self.splitters = self.find_splitters()
+        self.sources = self.find_sources()
+        self.final_splitters = self.find_final_splitters()
 
+    def find_start(self) -> Point:
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.values[i][j] == "S":
+                    return i, j
+        raise ValueError("start not found")
 
-def find_start(grid: list[list[str]]) -> Point:
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == "S":
-                return i, j
-    raise ValueError("start not found")
+    def find_splitters(self) -> list[Point]:
+        return [
+            (i, j)
+            for i in range(self.height)
+            for j in range(self.width)
+            if self.values[i][j] == "^"
+        ]
 
-
-def find_terminal_nodes_and_count_splits(
-    grid: list[list[str]], start: Point
-) -> tuple[list[Point], int]:
-    beams = set[Point]()
-    beams.add(start)
-    splits = 0
-    for _ in range(len(grid) - 1):
-        new_beams = set[Point]()
-        for i, j in beams:
-            if grid[i + 1][j] == ".":
-                new_beams.add((i + 1, j))
-            else:
-                splits += 1
-                new_beams.add((i + 1, j + 1))
-                new_beams.add((i + 1, j - 1))
-        beams = new_beams
-    return [(i + 1, j) for i, j in beams], splits
-
-
-def find_non_terminal_nodes(grid: list[list[str]]) -> list[Point]:
-    nodes = list[Point]()
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == "^" or grid[i][j] == "S":
-                nodes.append((i, j))
-    return nodes
-
-
-def dfs_count_paths(
-    neighbours: dict[Point, list[Point]], start: Point, end: Point
-) -> int:
-    stack = [(start, [start])]
-    count = 0
-    while stack:
-        node, path = stack.pop()
-        if node == end:
-            count += 1
-            continue
-        for neighbour in neighbours[node]:
-            if neighbour not in path:
-                stack.append((neighbour, path + [neighbour]))
-    return count
-
-
-def count_paths(
-    non_terminal_nodes: list[Point],
-    terminal_nodes: list[Point],
-    start: Point,
-) -> int:
-    all_nodes = non_terminal_nodes + terminal_nodes
-    all_nodes_by_j = defaultdict[int, list[Point]](list)
-    for i, j in all_nodes:
-        all_nodes_by_j[j].append((i, j))
-    for j in all_nodes_by_j.keys():
-        all_nodes_by_j[j].sort(key=lambda x: x[0])
-    neighbours = dict[Point, list[Point]]()
-    left, right = None, None
-    for i, j in non_terminal_nodes:
-        if (i, j) == start:
-            for node in all_nodes_by_j[j]:
-                if node[0] > i:
-                    neighbours[start] = [node]
+    def find_sources(self) -> dict[Point, list[Point]]:
+        sources = defaultdict[Point, list[Point]](list)
+        for splitter in self.splitters:
+            i, j = splitter
+            k = i - 1
+            right_under_source = False
+            while k >= 0:
+                if self.values[k][j] == ".":
+                    k -= 1
+                elif self.values[k][j] == "^":
                     break
-            continue
-        for node in all_nodes_by_j[j - 1]:
-            if node[0] > i:
-                left = node
-                break
-        for node in all_nodes_by_j[j + 1]:
-            if node[0] > i:
-                right = node
-                break
-        assert left is not None
-        assert right is not None
-        neighbours[(i, j)] = [left, right]
-    for node in terminal_nodes:
-        neighbours[node] = []
-    count = 0
-    for end in terminal_nodes:
-        count += dfs_count_paths(neighbours, start, end)
-    return count
+                else:  # S
+                    sources[(i, j)].append((k, j))
+                    right_under_source = True
+                    break
+            if right_under_source:
+                continue
+            for p in range(i - 1, k, -1):
+                if self.values[p][j + 1] == "^":
+                    sources[(i, j)].append((p, j + 1))
+                if self.values[p][j - 1] == "^":
+                    sources[(i, j)].append((p, j - 1))
+        return dict(sources)
+
+    @cache
+    def splits(self, splitter: Point) -> bool:
+        if splitter not in self.sources:
+            return False
+        if self.sources[splitter] == [self.start]:
+            return True
+        return any(self.splits(splitter) for splitter in self.sources[splitter])
+
+    def find_final_splitters(self) -> list[Point]:
+        final_splitters = list[Point]()
+        for splitter in self.splitters:
+            final = True
+            i, j = splitter
+            for k in range(i + 1, self.height):
+                if self.values[k][j + 1] == "^" or self.values[k][j - 1] == "^":
+                    final = False
+                    break
+            if final:
+                final_splitters.append(splitter)
+        return final_splitters
+
+
+def part1(grid: Grid) -> int:
+    return sum(grid.splits(splitter) for splitter in grid.splitters)
 
 
 def main() -> None:
-    grid = read_grid("example.txt")
-    start = find_start(grid)
-    terminal_nodes, splits = find_terminal_nodes_and_count_splits(grid, start)
-    assert splits == 21
-    non_terminal_nodes = find_non_terminal_nodes(grid)
-    assert count_paths(non_terminal_nodes, terminal_nodes, start) == 40
+    grid = Grid("example.txt")
+    assert part1(grid) == 21
 
-    grid = read_grid("input.txt")
-    start = find_start(grid)
-    terminal_nodes, splits = find_terminal_nodes_and_count_splits(grid, start)
-    assert splits == 1566
-    non_terminal_nodes = find_non_terminal_nodes(grid)
-    count_paths(non_terminal_nodes, terminal_nodes, start)
+    grid = Grid("input.txt")
+    assert part1(grid) == 1566
+
     print("All tests passed.")
 
 
